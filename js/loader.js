014258b1a678255aaaa921ffc4de8119299c3e0e
guy-blanchfield <98840904+guy-blanchfield@users.noreply.github.com>
2022-04-10 23:02:08 +0100

    Added very minimal error handling for promises

--- js/loader.js
+++ js/loader.js
@@ -1,37 +1,14 @@
 /* make sure the fonts and the first image are loaded before displaying the hero stuff */
 
 const firstImg =  document.querySelector('[data-src="../img/content/png/bls_home_crop.png"]');
-
-console.log(firstImg.getAttribute('data-src'));
-
-/*
-
-function checkImageLoaded(img) {
-
-    return new Promise(function (resolve, reject) {
-
-        img.onload = function () {
-            // console.log(img.src + " is loaded!");
-            resolve();
-        }
-
-    });
-}
-
-
-checkImageLoaded(firstImg).then(() => {
-
-    console.log("The promise is resolved");
-
-    }
-
-);
-*/
+// const firstImg =  document.querySelector('#bls-img');
+// console.log(firstImg.getAttribute('data-src'));
 
 const firstImgPromise = new Promise(function (resolve, reject) {
 
     firstImg.onload = function () {
         console.log(firstImg.src + " is loaded!");
+        // return resolve();
         resolve();
     }
 
@@ -41,17 +18,28 @@ const fontsPromise = new Promise(function (resolve, reject) {
 
     document.fonts.ready.then(function() {
         console.log("Fonts are ready!");
+        displayLogoIcon();
         resolve();
     });
 
 });
 
-Promise.all([firstImgPromise, fontsPromise]).then((values) => {
-    //console.log(values);
+Promise.all([firstImgPromise, fontsPromise]).then(() => {
     console.log("The promises are resolved");
-    displayHeroContent();
-    displayLogoIcon();
+    // display the hero content with animation
+    displayHeroContent(true);
+    // should probably do the displaylogoIcon() as soon as the fonts are loaded
+    // moved it into the fonts.ready.then()
+    // displayLogoIcon();
+}).catch(() => {
+
+    // if something goes wrong just make the hero
+    // content visible without the animation
+    displayHeroContent(false);
+
 });
+
+// no error handling for now
 
 function displayLogoIcon() {
 
@@ -64,31 +52,33 @@ function displayLogoIcon() {
 
 }
 
-function displayHeroContent() {
+function displayHeroContent(animation) {
 
     const heroContent = document.querySelector('.hero-content');
-    const heroH2Spans = document.querySelectorAll('.hero-content span');
-    
-    console.log(heroH2Spans);
 
-    const heroH2SpanClasses = ['greeting-hi', 'greeting-im', 'greeting-guy'];
+    if (animation) {
+        
+        const heroH2Spans = document.querySelectorAll('.hero-content span');
+        const heroH2SpanClasses = ['greeting-hi', 'greeting-im', 'greeting-guy'];
+        const heroContentPara = document.querySelector('.hero-content p');
 
-    const heroContentPara = document.querySelector('.hero-content p');
+        // add the fade-in animation classes to the h2 spans
+        heroH2Spans.forEach((span, index) => {
+            if (!span.classList.contains(heroH2SpanClasses[index])) {
+                span.classList.add(heroH2SpanClasses[index]);
+                console.log('adding class ' + heroH2SpanClasses[index]);
+            }
+        });
 
-    // add the fade-in animation classes to the h2 spans
-    heroH2Spans.forEach((span, index) => {
-        if (!span.classList.contains(heroH2SpanClasses[index])) {
-            span.classList.add(heroH2SpanClasses[index]);
-            console.log('adding class ' + heroH2SpanClasses[index]);
+        // add the typewiter class to hero p
+        if (!heroContentPara.classList.contains('typewriter')) {
+            heroContentPara.classList.add('typewriter');
+            // console.log('add class ready!');
         }
-    });
 
-    // add the typewiter class to hero p
-    if (!heroContentPara.classList.contains('typewriter')) {
-        heroContentPara.classList.add('typewriter');
-        // console.log('add class ready!');
     }
-
+    
+    // if animation != true just make the hero content visible
     if (!heroContent.classList.contains('ready')) {
         heroContent.classList.add('ready');
         // console.log('add class ready!');
